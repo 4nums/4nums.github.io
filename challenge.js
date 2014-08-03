ae617909580e64a58a71a44cb0c5de4bd01dc5d2
4nums <24thry@gmail.com>
2014-08-03 09:54:17 -0400

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -497,12 +497,12 @@ function tickclock(){
  
 
 function game_draw(isclock){ // 0) status1, 1) game_type, 2) time, time_left, 3) solved, unsolved, 4) this game history
-	draw_text(Array(width/100,height *1.05), "4shu.net今日高难24点,微信:4shu",  "#ddd",   + (width/16) +"px sans-serif");
+	draw_text(Array(width/100,height *1.05), "4shu.net今日高难24点,微信:4shu",  "#000",   + (width/16) +"px sans-serif");
 	if (status1 == 6)
 	{	var scoretext ="今天的高难24点你用时"+ (game_tick1/100)+  "秒";
 		draw_rect(Array(0,0, width, height), "#fff", 0, "#000"); // clean the whole region
 		draw_text(Array(width/100,height *0.3), scoretext,  "#f00",  "bold " + (width/15) +"px sans-serif");
-		draw_text(Array(width/100,height *0.6), "点击这里去4shu.net看更多24点益智游戏",  "#ddd",  "bold " + (width/15) +"px sans-serif");
+		draw_text(Array(width/100,height *0.6), "点击这里去4shu.net看更多24点益智游戏",  "#00f",  "bold " + (width/15) +"px sans-serif");
 		return; 
 	}
 	if (status1 == 0){
