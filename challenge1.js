fcfeb9ec0f06edd869288c38f0a52605ca169bdf
4nums <24thry@gmail.com>
2014-08-06 17:55:59 -0400

    Update challenge1.js

--- challenge1.js
+++ challenge1.js
@@ -497,7 +497,7 @@ function tickclock(){
  
 
 function game_draw(isclock){ // 0) status1, 1) game_type, 2) time, time_left, 3) solved, unsolved, 4) this game history
-	draw_text(Array(width/100,height *1.05), "4shu.net今日高难24点,微信:4shu",  "#555",   + (width/16) +"px sans-serif");
+	draw_text(Array(width/100,height *1.05), "4shu.net今日24点,微信:suan4shu",  "#555",   + (width/16) +"px sans-serif");
 	if (status1 == 6)
 	{	var scoretext ="今天的高难24点你用时"+ (game_tick1/100)+  "秒";
 		draw_rect(Array(0,0, width, height), "#fff", 0, "#000"); // clean the whole region
