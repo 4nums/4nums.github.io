da713640f92a7685075dcd77cad1d7dcf1a11915
4nums <24thry@gmail.com>
2014-08-06 14:18:34 -0400

    Update challenge1.js

--- challenge1.js
+++ challenge1.js
@@ -705,19 +705,22 @@ function handlekeydown(e){
 }
 function onclick1 (e){
 	var canvas = document.getElementById(canvasID);
+	 var rect = canvas.getBoundingClientRect();
        var x, y;
 	var mainbody = document.getElementsByTagName('body')[0]; //document.getElementById(mainbodyID);
 
-       if (e.pageX != undefined && e.pageY != undefined) {
-  		x = e.pageX;
-		y = e.pageY;
-       }
-       else {
+      // if (e.pageX != undefined && e.pageY != undefined) {
+  //		x = e.pageX;
+//		y = e.pageY;
+  //     }
+    //   else {
 		x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
 		y = e.clientY + document.body.scrollTop  + document.documentElement.scrollTop;
-       }
+      // }
        x -= (canvas.offsetLeft + 0* mainbody.offsetLeft);
        y -= (canvas.offsetTop + 0* mainbody.offsetTop);  
+       x= e.clientX - rect.left,
+        y= e.clientY - rect.top
 	switch (status1){
 		case  0: // on the init-game screen
  			var rect_arr = new Array(xywh_init, xywh_init1);
